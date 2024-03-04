41543d133207e1bde5cec4c40e2cb34137a3b87b
Yvette Achieng <achiengyvette@gmail.com>
2024-03-04 10:21:14 +0000

    adding file 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-	task = false;
-	task2 = true;
+	var task = false;
+	var task2 = true;
 	
 	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
+		task = true;
+		task2 = false;
 	}
-	return [var task, var task2];
+	return [task, task2];
 }
