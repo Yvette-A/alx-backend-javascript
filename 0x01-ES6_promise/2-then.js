87347e9376e453e43bd2ae9555e1772199a48fbc
Yvette Achieng <achiengyvette@gmail.com>
2024-03-06 09:02:16 +0000

    adding files to directory

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  const result = { status: 200, body: 'success' };
+  return promise
+    .then(() => result)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
